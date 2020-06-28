e830dff7b747fb9d8c2036dbbe557a21bcc186c0
Carlos Sanchez <carlo.s16@hotmail.com>
2020-06-28 00:32:06 -0400

    added clear and random button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useCallback, useRef } from 'react'
 import './App.css'
 import produce from 'immer'
 
-const numRows = 35
-const numCols = 75
+const numRows = 25
+const numCols = 25
 
 const genGrid = () => {
   const rows = []
@@ -11,7 +11,7 @@ const genGrid = () => {
     rows.push(Array.from(Array(numCols), () => 0))
   }
   return rows
-}
+} // Create initial grid, fill it in with all dead cells 
 
 const neighborFinder = [
   [1, 0], //North
@@ -28,8 +28,13 @@ function App() {
   const [grid, setGrid] = useState(() => {
     return genGrid()
   })
-  console.log('1: ' + grid)
-  useEffect(() => {
+
+  const [running, setRunning] = useState(false)
+
+  const runningRef = useRef(running)
+  runningRef.current = running
+
+  const CreateRandomGrid = () => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
       rows.push(
@@ -38,13 +43,14 @@ function App() {
         )
       )
     }
-
     setGrid(rows)
-  }, [])
-  
-  console.log('2: ' + grid)
+  }// Randomly fill in the grid
 
   const runSim = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+
     setGrid(currGrid => {
       return produce(currGrid, gridCopy => {
         for (let i = 0; i < numRows; i++) {
@@ -52,23 +58,22 @@ function App() {
             let neighborCount = 0
             neighborFinder.forEach(([x, y]) => {
               const newI = i + x
-              const newJ = j + y
-
+              const newJ = j + y // Searches for neighbors
               if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
                 neighborCount += currGrid[newI][newJ]
-              }
+              } // Checks to make sure that it doesn't go out of bounds
             })
-
+            /* Makes the cell alive or dead depending on the cell */
             if (neighborCount < 2 || neighborCount > 3) gridCopy[i][j] = 0
             else if (currGrid[i][j] === 0 && neighborCount === 3) gridCopy[i][j] = 1
           }
         }
       })
     })
-    setTimeout(runSim, 100)
+    setTimeout(runSim, 500)
   }, [])
   return (
-    <>
+    <div className='contentHolder'>
       <div className="App" style={{ display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`, backgroundColor: 'black'}}>
         {grid.map((rows, i) => 
           rows.map((col, j) => 
@@ -84,9 +89,16 @@ function App() {
             style={{width: 20, height: 20, backgroundColor: grid[i][j] ? `#${(Math.random()*0xFFFFFF<<0).toString(16)}` : undefined,
             border: 'solid 1px black'}} />))}
       </div>
-      <button onClick={() => runSim()}>Start</button>
-    </>
-  );
+      <button onClick={() => {
+        setRunning(!running)
+        if (!running) runningRef.current = true
+        runSim()
+        }
+      }>{running ? 'Stop' : 'Start'}</button>
+      <button onClick={() => setGrid(genGrid())}>Clear</button>
+      <button onClick={() => CreateRandomGrid()}>Random</button>
+    </div>
+  )
 }
 
 export default App
